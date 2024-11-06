4b1047809da11cc159c9e32547afe9860f893c38
BelRom <bel.r.s@yandex.ru>
2024-11-06 07:24:31 +0000

    Убрал логи

--- notebooks/nampy_random_agent.py
+++ notebooks/nampy_random_agent.py
@@ -1,6 +1,4 @@
 import numpy as np
 
 def nampy_random_agent(observation, configuration):
-    result = np.random.randint(0, configuration.signs)
-    print(f"nampy_random_agent result {result}")
-    return result
+    return np.random.randint(0, configuration.signs)
--- notebooks/seed_time_random_agent.py
+++ notebooks/seed_time_random_agent.py
@@ -4,5 +4,4 @@ import time
 def seed_time_random_agent(observation, configuration):
     random.seed(time.time())
     result = random.randrange(0, configuration.signs)    
-    print(f"seed_time_random_agent {result}")
     return result
